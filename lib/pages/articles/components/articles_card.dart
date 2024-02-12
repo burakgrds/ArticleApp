ef490b9f9a7aaafab9515d00e804fd36b11f3dc5
burakgrds <golburak@gmail.com>
2024-02-12 23:13:30 +0300

    pubspec upgrade and minor fixes

--- lib/pages/articles/components/articles_card.dart
+++ lib/pages/articles/components/articles_card.dart
@@ -14,50 +14,63 @@ class ArticlesCard extends StatelessWidget {
 
     return now_1w.isBefore(
             DateTime.parse(model.articlesModel.results[index].publishedDate!))
-        ? Card(
-            color: Colors.grey,
-            child: Column(
-              children: [
-                ListTile(
-                  contentPadding: const EdgeInsets.all(12),
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Image.network(model.articlesModel.results[index]
-                        .media!.first.mediaMetadata!.first.url!),
-                  ),
-                  onTap: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ArticleDetailsView(
-                        index: index,
-                      ),
+        ? model.articlesModel.results[index].media!.isEmpty
+            ? Container()
+            : InkWell(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ArticleDetailsView(
+                      index: index,
                     ),
                   ),
-                  trailing: const Icon(Icons.chevron_right),
-                  title: Text(model.articlesModel.results[index].title ?? ""),
-                  subtitle:
-                      Text(model.articlesModel.results[index].byline ?? ""),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10, bottom: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Icon(
-                        Icons.calendar_month,
-                        color: Colors.white,
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        model.articlesModel.results[index].publishedDate ?? "",
-                        style: const TextStyle(color: Colors.white),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ))
+                child: Card(
+                    color: Colors.grey,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          contentPadding: const EdgeInsets.all(12),
+                          leading: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(model
+                                .articlesModel
+                                .results[index]
+                                .media!
+                                .first
+                                .mediaMetadata!
+                                .first
+                                .url!),
+                          ),
+                          trailing: const Icon(Icons.chevron_right),
+                          title: Text(
+                              model.articlesModel.results[index].title ?? ""),
+                          subtitle: Text(
+                              model.articlesModel.results[index].byline ?? ""),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10, bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              const Icon(
+                                Icons.calendar_month,
+                                color: Colors.white,
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                model.articlesModel.results[index]
+                                        .publishedDate ??
+                                    "",
+                                style: const TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
+              )
         : Container();
   }
 }
